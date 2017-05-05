fd20e493d7cb047736a97645b9f25762c8c8959f
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-05-05 17:24:29 +0100

    Adds feature: password confirmation

--- spec/features/adding_user_accounts_spec.rb
+++ spec/features/adding_user_accounts_spec.rb
@@ -7,4 +7,9 @@ feature 'Adding user accounts' do
   scenario 'user count changes by +1 after signing up' do
     expect { sign_up }.to change(User, :count).by(1)
   end
+
+  scenario 'password requires confirmation' do
+    sign_up(password_confirmation: 'scrt')
+    expect { sign_up(password_confirmation: 'scrt') }.to_not change(User, :count)
+  end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -4,9 +4,12 @@ def add_a_link
   fill_in :site_url, with: 'https://www.youtube.com/'
 end
 
-def sign_up
+def sign_up(email: 'joe@example.com',
+            password: 'secret',
+            password_confirmation: 'secret')
   visit '/users/new'
-  fill_in :email, with: 'joe@example.com'
-  fill_in :password, with: 'secret'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up'
 end
